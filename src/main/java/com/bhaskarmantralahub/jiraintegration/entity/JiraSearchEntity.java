0f31c65de538795fe3b232692116ea3bd5ec2d0a
BhaskarMantralaHub <97362616+BhaskarMantralaHub@users.noreply.github.com>
2023-06-14 21:38:41 -0700

    repository

--- src/main/java/com/bhaskarmantralahub/jiraintegration/entity/JiraSearchEntity.java
+++ src/main/java/com/bhaskarmantralahub/jiraintegration/entity/JiraSearchEntity.java
@@ -1,52 +1,52 @@
-//package com.bhaskarmantralahub.jiraintegration.entity;
-//
-//import lombok.*;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "jira_search", schema = "jira-integration")
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@ToString
-//public class JiraSearchEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private long id;
-//
-//    @Column(name = "issueId")
-//    private String issueId;
-//
-//    @Column(name = "issueType")
-//    private String issueType;
-//
-//    @Column(name = "project")
-//    private String project;
-//
-//    @Column(name = "priority")
-//    private String priority;
-//
-//    @Column(name = "assignee")
-//    private String assignee;
-//
-//    @Column(name = "reporter")
-//    private String reporter;
-//
-//    @Column(name = "updatedAt")
-//    private String updatedAt;
-//
-//    @Column(name = "createdAt")
-//    private String createdAt;
-//
-//    @Column(name = "currentStatus")
-//    private String currentStatus;
-//
-//    @Column(name = "searchUrl")
-//    private String searchUrl;
-//
-//    @Column(name = "description")
-//    private String description;
-//}
+package com.bhaskarmantralahub.jiraintegration.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "jira_search", schema = "jira-integration")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class JiraSearchEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "issueId", unique = true)
+    private String issueId;
+
+    @Column(name = "issueType")
+    private String issueType;
+
+    @Column(name = "project")
+    private String project;
+
+    @Column(name = "priority")
+    private String priority;
+
+    @Column(name = "assignee")
+    private String assignee;
+
+    @Column(name = "reporter")
+    private String reporter;
+
+    @Column(name = "updatedAt")
+    private String updatedAt;
+
+    @Column(name = "createdAt")
+    private String createdAt;
+
+    @Column(name = "currentStatus")
+    private String currentStatus;
+
+    @Column(name = "searchUrl", unique = true)
+    private String searchUrl;
+
+    @Column(name = "description")
+    private String description;
+}
--- src/main/java/com/bhaskarmantralahub/jiraintegration/repository/JiraSearchRepository.java
+++ src/main/java/com/bhaskarmantralahub/jiraintegration/repository/JiraSearchRepository.java
@@ -1,11 +1,9 @@
-//package com.bhaskarmantralahub.jiraintegration.repository;
-//
-//import com.bhaskarmantralahub.jiraintegration.entity.JiraSearchEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-//
-//import java.util.List;
-//
-//@RepositoryRestResource(collectionResourceRel = "JiraSearch", path ="JiraSearch")
-//public class JiraSearchRepository extends JpaRepository<JiraSearchEntity, Long> {
-//}
+package com.bhaskarmantralahub.jiraintegration.repository;
+
+import com.bhaskarmantralahub.jiraintegration.entity.JiraSearchEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "JiraSearch", path ="JiraSearch")
+public interface JiraSearchRepository extends JpaRepository<JiraSearchEntity, Long> {
+}
